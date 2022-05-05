5f5e7b035cb80e00d6b8c9f7867850db48aa55ba
Suneom <dkxl0701@gmail.com>
2022-05-05 23:14:16 +0900

    Section2: Creating new project

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 엄태양 on 2022/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
