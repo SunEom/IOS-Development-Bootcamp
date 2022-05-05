a0b3e5bab4ff36f09744054e838d921175113095
Suneom <dkxl0701@gmail.com>
2022-05-06 00:49:35 +0900

    Section3

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by 엄태양 on 2022/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
